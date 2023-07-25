ae216f4c70204e2b310a829b1aa25227b608d218
Sullivan Myer <slucasmyer@icloud.com>
2023-07-24 22:58:28 -0400

    build

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,11 +4,9 @@ import { useState, useEffect } from 'react';
 import { AppBar, Drawer, Slide, useScrollTrigger } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import { useRouter, usePathname } from "next/navigation";
-import { signIn, signOut, useSession } from "next-auth/react"
 import Button from './Button';
 
 export default function Navigation() {
-  const { data: session, status } = useSession()
   const trigger = useScrollTrigger();
   const router = useRouter();
   const pathName = usePathname();
@@ -27,15 +25,7 @@ export default function Navigation() {
           <Link href={`/marketplace`}><h1 className={`transition-all duration-500 text-2xl text-bold hover:underline hover:scale-125`}>MARKETPLACE</h1></Link>
           <Link href={`/account`}><h1 className={`transition-all duration-500 text-2xl text-bold hover:underline hover:scale-125`}>ACCOUNT</h1></Link>
           <Link href={`/about`}><h1 className={`transition-all duration-500 text-2xl text-bold hover:underline hover:scale-125`}>ABOUT</h1></Link>
-          <Link
-            href={`/api/auth/signin`}
-            onClick={(e) => {
-              e.preventDefault()
-              signIn()
-            }}
-          >
-            <h1 className={`transition-all duration-500 text-2xl text-bold hover:underline hover:scale-125`}>SIGN-IN</h1>
-          </Link>
+          
         </nav>
         <nav className={`flex md:hidden justify-between my-2 mx-2`}>
           <Link href={`/`}><h1 className={`text-2xl text-bold`}>Employable.Team</h1></Link>
